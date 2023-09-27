720aecbdcdbaea59ec0861bbbeccf3f45a9d4b36
Ahnaf Mubashshir Mobin <99656046+AhnafMubashshir@users.noreply.github.com>
2023-09-27 07:41:39 +0600

    Initial commit

---
+++ game.js
@@ -0,0 +1,14 @@
+import { generateBoard } from './boardGeneration.js';
+import { updatePlayerPosition, movePlayer } from './gameEngine.js';
+import * as Globals from './global.js';
+
+const movements = Globals.neighbourCells;
+
+
+function startGame () {
+    generateBoard(7, 13);
+    updatePlayerPosition();
+    movePlayer('right');
+}
+
+startGame();
